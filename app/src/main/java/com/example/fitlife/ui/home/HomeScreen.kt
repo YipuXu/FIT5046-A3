44f190d35eeed1d452e34cf4ed157063324eb4a1
Guanhghong Zi <guanghongzi00@gmail.com>
2025-05-21 23:10:32 +1000

    add basic music api

--- app/src/main/java/com/example/fitlife/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/fitlife/ui/home/HomeScreen.kt
@@ -63,6 +63,7 @@ fun HomeScreen(
             WelcomeSection()
 //            WeeklySummaryCard()
 //            TodayPlanCard()
+            MusicPlayerScreen()
             ExerciseRecommendation()
 //            MusicPlayerSection()
         }
